a67b57fa2d07a9e8b52fd1175a2f7d45b95b1090
pentzto89279 <pentzto89279@th-nuernberg.de>
2022-12-01 22:28:52 +0100

    Fix

--- olbaid-mortel-7720/MVVM/View/LevelWrapperView.xaml.cs
+++ olbaid-mortel-7720/MVVM/View/LevelWrapperView.xaml.cs
@@ -18,7 +18,7 @@ namespace olbaid_mortel_7720.MVVM.View
     private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
     {
       double x = GlobalVariables.MaxX * PlayerControl.ActualWidth / ScalingViewBox.ActualWidth;
-      double y = GlobalVariables.MaxX * PlayerControl.ActualWidth / ScalingViewBox.ActualWidth;
+      double y = GlobalVariables.MaxY * PlayerControl.ActualWidth / ScalingViewBox.ActualWidth;
 
       GlobalVariables.MaxX = Convert.ToInt32(x);
       GlobalVariables.MaxY = Convert.ToInt32(y);
