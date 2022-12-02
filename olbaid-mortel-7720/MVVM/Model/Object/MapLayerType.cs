bd4eea3aa06ba4b42aa09019d1f357a7f66bfd3e
kapellnerti88451 <kapellnerti88451@th-nuernberg.de>
2022-12-02 08:04:54 +0100

    Add graphic sizes as collisionboxes for the walls

--- olbaid-mortel-7720/MVVM/Model/Object/MapLayerType.cs
+++ olbaid-mortel-7720/MVVM/Model/Object/MapLayerType.cs
@@ -4,6 +4,7 @@
   {
     public static readonly string OUTER_WALL = "outer wall";
     public static readonly string INNER_WALL = "Inner Wall";
+    public static readonly string STAIRS = "Stairs";
     public static readonly string FLOOR = "Floor";
     public static readonly string FURNITURE = "Furniture";
     public static readonly string DOOR = "outer Door";
